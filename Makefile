30c9f908e66f4cc2ba7059bc5e91f0d6f5f8d4c9
Filipe Azevedo <filipe@azevedo.io>
2023-07-18 15:56:26 +0100

    add substream-base subgraph

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-ENDPOINT ?= testnet.near.streamingfast.io:443
-START_BLOCK ?= 55000000
+ENDPOINT ?= mainnet.near.streamingfast.io:443
+START_BLOCK ?= 96764162
 STOP_BLOCK ?= +100
 
 .PHONY: build
@@ -8,7 +8,7 @@ build:
 
 .PHONY: run
 run: build
-	substreams run -e $(ENDPOINT) substreams.yaml map_block -s $(START_BLOCK) -t $(STOP_BLOCK)
+	substreams run -e $(ENDPOINT) substreams.yaml graph_out -s $(START_BLOCK) -t $(STOP_BLOCK)
 
 .PHONY: gui
 gui: build
@@ -21,3 +21,15 @@ protogen:
 .PHONY: pack
 pack: build
 	substreams pack substreams.yaml
+
+.PHONY: deploy_local
+deploy_local: pack
+	mkdir build 2> /dev/null || true
+	bun x graph build --ipfs http://localhost:5001 subgraph.yaml
+	bun x graph create map_block --node http://127.0.0.1:8020
+	bun x graph deploy --node http://127.0.0.1:8020 --ipfs http://127.0.0.1:5001 --version-label v0.0.1 map_block subgraph.yaml
+
+.PHONY: undeploy_local
+undeploy_local:
+	graphman --config "$(GRAPH_CONFIG)" drop --force uniswap_v3
+
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use pb::{
     receipts::v1::{Receipt, Receipts},
     sf::near::r#type::v1::Block,
 };
+use substreams_entity_change::pb::entity::EntityChanges;
 
 #[substreams::handlers::map]
 fn map_block(blk: Block) -> Result<Receipts, substreams::errors::Error> {
@@ -17,13 +18,13 @@ fn map_block(blk: Block) -> Result<Receipts, substreams::errors::Error> {
 
         for receipt in chunk.receipts {
             out.chunk_receipts.push(Receipt {
-                id: to_base58(receipt.receipt_id.unwrap().bytes.as_slice()),
+                id: hex::encode(receipt.receipt_id.unwrap().bytes.as_slice()),
             })
         }
 
         for exec_outcome in shard.receipt_execution_outcomes {
             out.outcome_receipts.push(Receipt {
-                id: to_base58(
+                id: hex::encode(
                     exec_outcome
                         .receipt
                         .unwrap()
@@ -39,6 +40,18 @@ fn map_block(blk: Block) -> Result<Receipts, substreams::errors::Error> {
     Ok(out)
 }
 
-fn to_base58(bytes: &[u8]) -> String {
-    bs58::encode(bytes).into_string()
+#[substreams::handlers::map]
+fn graph_out(blk: Block) -> Result<EntityChanges, substreams::errors::Error> {
+    let mut out = EntityChanges::default();
+
+    let hex = hex::encode(&blk.header.as_ref().unwrap().hash.as_ref().unwrap().bytes);
+
+    out.push_change(
+        "Block",
+        &hex,
+        blk.header.unwrap().height,
+        substreams_entity_change::pb::entity::entity_change::Operation::Create,
+    );
+
+    Ok(out)
 }
